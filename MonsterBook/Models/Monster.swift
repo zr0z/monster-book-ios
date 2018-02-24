795e9d59a8dbc855cc9749b108d2b0a0e60394fe
Zaidin Amiot <zr0z@me.com>
2018-02-24 17:15:05 +0100

    Add codable Monster model

---
+++ MonsterBook/Models/Monster.swift
@@ -0,0 +1,22 @@
+//
+//  Monster.swift
+//  MonsterBook
+//
+//  Created by Zaidin on 2018-02-24.
+//  Copyright © 2018 zr0z. All rights reserved.
+//
+
+import Foundation
+
+struct Monster: Codable {
+    let name: String
+    let specie: String
+    let ailments: [String]
+    let weakness: [String]
+}
+// MARK: CustomStringConvertible
+extension Monster: CustomStringConvertible {
+    var description: String {
+        return "<\(specie): \(name)>"
+    }
+}
